e774e5db66f62d91a4dbe245ecd38205a0b9fae6
Viktor Viskov <carrergt@gmail.com>
2021-05-21 22:38:18 +0200

    Finish with out normal interface and with TODO task

--- engine/connection/connection.cs
+++ engine/connection/connection.cs
@@ -3,7 +3,8 @@ using System;
 using MySql.Data.MySqlClient;
 
 // namespace
-namespace engine.connection {
+namespace engine.connection
+{
 
     // class for open connection
     class MysqlConnection
@@ -13,7 +14,7 @@ namespace engine.connection {
         MySqlConnection connection;
 
         // constructor
-        public MysqlConnection(string settings = "server=10.0.0.2;userid=root;password=dbnmjr031193;") 
+        public MysqlConnection(string settings = "server=10.0.0.2;userid=root;password=dbnmjr031193;")
         {
             this.settingsSting = settings;
 
@@ -22,24 +23,27 @@ namespace engine.connection {
         }
 
         // method for open connection
-        public void Open () {
+        public void Open()
+        {
             connection = new MySqlConnection(settingsSting);
             connection.Open();
         }
 
         // method for send sql requst
-        public void I (string sqlRequest) {
+        public void I(string sqlRequest)
+        {
             // make command
-            MySqlCommand cmd = new MySqlCommand(sqlRequest ,this.connection);
-            
+            MySqlCommand cmd = new MySqlCommand(sqlRequest, this.connection);
+
             //send command
             cmd.ExecuteNonQuery();
         }
 
         // method for send sql request and get response
-        public string IO (string sqlRequest){
+        public string IO(string sqlRequest)
+        {
             // make command
-            MySqlCommand cmd = new MySqlCommand(sqlRequest ,this.connection);
+            MySqlCommand cmd = new MySqlCommand(sqlRequest, this.connection);
 
             // send command and get data
             MySqlDataReader response = cmd.ExecuteReader();
@@ -47,18 +51,46 @@ namespace engine.connection {
             // buffer string 
             string bufferString = "";
 
-            while (response.Read()){
-                bufferString += $"{response[0]},";
+            // getting data
+            while (response.Read())
+            {
+
+                // Get columns count
+                int columns = response.FieldCount;
+
+                // loop for get all data from all columns
+                for (int i = 0; i < columns; i++)
+                {
+                    // add one column
+                    bufferString += $"{response[i]}";
+
+                    // add column separator
+                    if (i + 1 != columns)
+                    {
+                        bufferString += ",";
+                    }
+                }
+                // add row separator
+                bufferString += ";";
+            }
+
+            // delete last separator
+            if (bufferString.Length > 0)
+            {
+                bufferString = bufferString.Substring(0, bufferString.Length - 1);
             }
 
             // close connection
             response.Close();
 
+            // result
             return bufferString;
         }
 
         // show connection version
-        public string Version() {
+        public string Version()
+        {
+            // return result
             return connection.ServerVersion;
         }
     }
--- engine/ui/ui.cs
+++ engine/ui/ui.cs
@@ -29,7 +29,6 @@ namespace engine.ui
         private void Start() {
             // main interface loop
             while (isRunning) {
-
                 // show controll
                 ShowControll();
 
@@ -62,16 +61,127 @@ namespace engine.ui
                         //clean console
                         Console.Clear();
 
-                        connection.IO("SHOW DATABASES");
+                        // print result
+                        Console.WriteLine(connection.IO("SHOW DATABASES").Replace(";","\n"));
+
+                        // pause
+                        Pause();
+                        break;
+                    // show all tables
+                    case ConsoleKey.D2:
+                        //clean console
+                        Console.Clear();
+
+                        // show databases
+                        Console.WriteLine(connection.IO("SHOW DATABASES").Replace(";","\n") + "\n");
+
+                        // Print message
+                        Console.Write("Database name: ");
+
+                        // try exeption
+                        try {
+                            // print result
+                            Console.WriteLine(connection.IO($"SHOW TABLES FROM {Console.ReadLine().Trim()}").Replace(";","\n"));
+                        }
+                        //error message
+                        catch {
+                            Console.WriteLine("Database not found");
+                        }
 
                         // pause
                         Pause();
                         break;
 
-                    // creating database
-                    case ConsoleKey.D3: 
+                    // show table
+                    case ConsoleKey.D3:
                         //clean console
                         Console.Clear();
+
+                        // show databases
+                        Console.WriteLine(connection.IO("SHOW DATABASES").Replace(";","\n") + "\n");
+
+                        // Print message
+                        Console.Write("Database name: ");
+
+                        // get database name from user
+                        string databaseNameFour = Console.ReadLine().Trim(); //TODO
+
+                        //clean console
+                        Console.Clear();
+
+                        // try exeption
+                        try {
+                            // print result
+                            Console.WriteLine(connection.IO($"SHOW TABLES FROM {databaseNameFour}").Replace(";","\n") + "\n");
+                        }
+                        //error message
+                        catch {
+                            Console.WriteLine("Database not found");
+
+                            //pause
+                            Pause();
+
+                            break;
+                        }
+
+                        // Print message
+                        Console.Write("Table name: ");
+
+                        // get table name
+                        string tableNameFour = Console.ReadLine().Trim(); //TODO
+
+                        //clean console
+                        Console.Clear();
+
+                        // try exeption
+                        try {
+                            // print result
+                            Console.WriteLine(connection.IO($"SELECT * FROM {databaseNameFour}.{tableNameFour}"));
+                        }
+                        //error message
+                        catch {
+                            Console.WriteLine("Table not found");
+                        }
+
+                        // show table
+
+                        //pause
+                        Pause();
+
+                        break;
+
+                    // creating database
+                    case ConsoleKey.D4: 
+                        //clean console
+                        Console.Clear();
+
+                        // Print message
+                        Console.Write("Database name: ");
+
+                        // try exeption
+                        try {
+                            // make request
+                            connection.I($"CREATE DATABASE {Console.ReadLine().Trim()}");
+
+                            // print status message
+                            Console.WriteLine("Database created!");
+                        }
+                        catch {
+                            // print status message
+                            Console.WriteLine("Database is alredy exist!");
+                        }
+
+                        // pause
+                        Pause();
+                        break;
+                    
+                    // creating table
+                    case ConsoleKey.D5: 
+                        //clean console
+                        Console.Clear();
+
+                        // show databases
+                        Console.WriteLine(connection.IO("SHOW DATABASES").Replace(";","\n") + "\n");
 
                         // Print message
                         Console.Write("Database name: ");
@@ -79,8 +189,242 @@ namespace engine.ui
                         // get database name from user
                         string databaseName = Console.ReadLine().Trim();
 
-                        // make request
-                        connection.I($"CREATE DATABASE {databaseName}");
+                        // try exeption
+                        try {
+                            // print result
+                            Console.WriteLine(connection.IO($"SHOW TABLES FROM {databaseName}").Replace(";","\n") + "\n");
+                        }
+                        //error message
+                        catch {
+                            Console.WriteLine("Database not found");
+
+                            //pause
+                            Pause();
+
+                            break;
+                        }
+
+                        //clean console
+                        Console.Clear();
+
+                        // Print message
+                        Console.Write("Table name: ");
+
+                        // get table name
+                        string tableName = Console.ReadLine().Trim();
+
+                        // Print message
+                        Console.Write("Amount colums: ");
+
+                        // amount colums
+                        int amountColums = int.Parse(Console.ReadLine());
+
+                        string sqlRequest = $"CREATE TABLE {databaseName}.{tableName} (";
+
+                        // get datatypes for colums
+                        for (int i = 0; i < amountColums; i++){
+                            // print message
+                            Console.Write($"Column {i + 1} name: ");
+
+                            // add column name
+                            sqlRequest += Console.ReadLine().Trim() + " ";
+
+                            // print message
+                            Console.Write($"Column {i + 1} type: ");
+
+                            // add column type
+                            sqlRequest += Console.ReadLine().Trim();
+
+                            // add separator if not last
+                            if (i + 1 < amountColums) {
+                                sqlRequest += ",";
+                            }
+                        }
+
+                        // finish sql request
+                        sqlRequest += ")";
+
+                        // try exeption
+                        try {
+                            // make request
+                            connection.I(sqlRequest);
+
+                            // print status message
+                            Console.WriteLine("Table created!");
+                        }
+                        catch {
+                            // print status message
+                            Console.WriteLine("Error. Check inputed data. Table not created.");
+                        }
+
+                        // pause
+                        Pause();
+                        break;
+
+                    case ConsoleKey.D6:
+                        //clean console
+                        Console.Clear();
+
+                        // show databases
+                        Console.WriteLine(connection.IO("SHOW DATABASES").Replace(";","\n") + "\n");
+
+                        // Print message
+                        Console.Write("Database name: ");
+
+                        // get database name from user
+                        string databaseNameTwo = Console.ReadLine().Trim(); // TODO change to method
+
+                        //clean console
+                        Console.Clear();
+
+                        // try exeption
+                        try {
+                            // print result
+                            Console.WriteLine(connection.IO($"SHOW TABLES FROM {databaseNameTwo}").Replace(";","\n") + "\n");
+                        }
+                        //error message
+                        catch {
+                            Console.WriteLine("Table not found");
+
+                            //pause
+                            Pause();
+
+                            break;
+                        }
+
+
+                        // Print message
+                        Console.Write("Table name: ");
+
+                        // get table name
+                        string tableNameTwo = Console.ReadLine().Trim(); // TODO change to method
+
+                        //clean console
+                        Console.Clear();
+
+                        // try exept
+                        try {
+                            string[] responseString = connection.IO($"DESCRIBE {databaseNameTwo}.{tableNameTwo}").Split(";");
+                            // values string
+                            string values = "";
+
+                            // get data from user
+                            foreach (var item in responseString)
+                            {
+                                // separating data
+                                string[] dataArr = item.Split(",");
+
+                                // print message
+                                Console.Write($"{dataArr[0]}({dataArr[1]}): ");
+
+                                // add to values
+                                values += "'" + Console.ReadLine() + "',";
+                            }
+
+                            // delete last coma
+                            values = values.Substring(0, values.Length - 1);
+
+                            // sql request string
+                            connection.I($"INSERT INTO {databaseNameTwo}.{tableNameTwo} VALUES ({values})");
+
+                            // status message
+                            Console.WriteLine("Data wat added");
+
+                            // pause
+                            Pause();
+                        }
+                        catch {
+                            //message
+                            Console.WriteLine("Incorrect input. Try again");
+
+                            // pause
+                            Pause();
+
+                            break;
+                        }
+                        break;
+
+                    // delete table
+                    case ConsoleKey.D7:
+                        //clean console
+                        Console.Clear();
+
+                        // show databases
+                        Console.WriteLine(connection.IO("SHOW DATABASES").Replace(";","\n") + "\n");
+
+                        // Print message
+                        Console.Write("Database name: ");
+
+                        // get database name from user
+                        string databaseNameThree = Console.ReadLine().Trim(); // TODO change to method
+
+                        //clean console
+                        Console.Clear();
+                        
+                        // try exeption
+                        try {
+                            // print result
+                            Console.WriteLine(connection.IO($"SHOW TABLES FROM {databaseNameThree}").Replace(";","\n") + "\n");
+                        }
+                        //error message
+                        catch {
+                            Console.WriteLine("Table not found");
+
+                            //pause
+                            Pause();
+
+                            break;
+                        }
+
+                        // Print message
+                        Console.Write("Table name: ");
+
+                        // get table name
+                        string tableNameThree = Console.ReadLine().Trim(); // TODO change to method
+
+                        // try exeption
+                        try {
+                            // make request
+                            connection.I($"DROP TABLE {databaseNameThree}.{tableNameThree}");
+
+                            // print status message
+                            Console.WriteLine($"Table {tableNameThree} from {databaseNameThree} deleted!");
+                        }
+                        catch {
+                            // print status message
+                            Console.WriteLine("Error. Check inputed data. Table not deleted.");
+                        }
+
+                        // pause
+                        Pause();
+                        break;
+
+                    // delete database
+                    case ConsoleKey.D8:
+                        //clean console
+                        Console.Clear();
+
+                        // show databases
+                        Console.WriteLine(connection.IO("SHOW DATABASES").Replace(";","\n") + "\n");
+
+                        // Print message
+                        Console.Write("Database name: ");
+
+                        // try exeption
+                        try {
+                            // make request
+                            connection.I($"DROP DATABASE {Console.ReadLine().Trim()}");
+
+                            // print status message
+                            Console.WriteLine($"DATABASE deleted!");
+                        }
+                        catch {
+                            // print status message
+                            Console.WriteLine("Error. Check inputed data. Database not deleted.");
+                        }
+
+                        // pause
+                        Pause();
                         break;
 
                     case ConsoleKey.V:
@@ -93,12 +437,10 @@ namespace engine.ui
                         // pause
                         Pause();
                         break;
-
-
                 }
+
                 //clean console
                 Console.Clear();
-
             }
         }
 
@@ -108,18 +450,18 @@ namespace engine.ui
             Console.WriteLine("Press ESC to exit");
             Console.WriteLine("Press 1 to show all databases");
             Console.WriteLine("Press 2 to show all tables");
-            Console.WriteLine("Press 3 to create database");
-            Console.WriteLine("Press 4 to create table");
-            Console.WriteLine("Press 5 to insert data in table");
-            Console.WriteLine("Press 6 to delete table");
-            Console.WriteLine("Press 7 to delete database");
-            Console.WriteLine("Press 9 to change database");
+            Console.WriteLine("Press 3 to show table");
+            Console.WriteLine("Press 4 to create database");
+            Console.WriteLine("Press 5 to create table");
+            Console.WriteLine("Press 6 to insert data in table");
+            Console.WriteLine("Press 7 to delete table");
+            Console.WriteLine("Press 8 to delete database");
             Console.WriteLine("Press v to show DB version");
         }
 
         // pause
         public void Pause(){
-            Console.WriteLine("\nPress any key to continnue");
+            Console.WriteLine("\nPress any key to continue");
             Console.ReadKey();
         }
     }
